01d6fb219339dc084763bfbda0b8c39c79d6225a
nitinkanasnale <nitinkanasnale3@gmail.com>
2025-10-30 22:22:22 +0530

    Initial commit - Average programme

---
+++ Average.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Average {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number[A]: ");
+        double A = sc.nextDouble();
+        System.out.print("Enter second number[B]: ");
+        double B = sc.nextDouble();
+        System.out.print("Enter third number[C]: ");
+        double C = sc.nextDouble();
+        double average = (A + B + C) / 3;
+        System.out.println("The average of A, B and C is: " + average);
+    }
+}
+
