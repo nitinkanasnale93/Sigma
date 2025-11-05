9a741e1929e3042f2f9123ca2c3f13b89f346b96
nitinkanasnale <nitinkanasnale3@gmail.com>
2025-11-05 06:18:56 +0530

    Added Even number between 9 to 100

---
+++ Evenbet.java
@@ -0,0 +1,11 @@
+public class Evenbet {
+    public static void main(String[] args) {
+        for(int i = 10; i <= 100; i++) {
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+    
+
